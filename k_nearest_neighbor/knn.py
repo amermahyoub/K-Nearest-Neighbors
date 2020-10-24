e977b093455d3cdba9a0e50fd5bda596542e4059
ameralhomdy <ameralhomdy97@gmail.com>
2020-10-23 20:35:34 -0700

    Finished building knn from scratch

---
+++ k_nearest_neighbor/knn.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def euclidean_distance(x1, x2):
+    # calculating the euclidean distance for our dataset
+    return np.sqrt(np.sum((x1-x2)**2))
+
+def most_common(word_list):
+        # calculates the most common element in a list
+        return max(set(word_list), key = word_list.count)
+
+class KNN:
+
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit_model(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        # running prediction on every row and returning it as numpy array
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+
+    def _predict(self, x):
+        # compute distance between every column’s value
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        # get k nearest samples
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        # majority vote, most common class label
+        return most_common(k_nearest_labels)
+
+    def accuracy(y_true, y_pred):
+        # accuracy is calculated by counting how many right labels we got
+        # and dividing it by the total labels
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
